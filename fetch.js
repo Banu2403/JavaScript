c5690c1410cdac82a6f3f9246d3fbd9a4a24dd71
Banu2403 <banuisgenderli2011@gmail.com>
2025-04-25 20:47:48 -0700

    Add files via upload

---
+++ fetch.js
@@ -0,0 +1,20 @@
+searchForm.onsubmit = async e => {
+    e.preventDefault();
+    const q = pokemonInput.value.trim().toLowerCase();
+    if (!q) return errorMsg.textContent = "Enter a name!";
+    errorMsg.textContent = result.innerHTML = "";
+    try {
+      const r = await fetch(`https://pokeapi.co/api/v2/pokemon/${q}`);
+      if (!r.ok) throw "Not found!";
+      const d = await r.json();
+      result.innerHTML = `
+        <h2>${d.name}</h2>
+        <img src="${d.sprites.front_default}">
+        <p>Height: ${d.height}</p>
+        <p>Type: ${d.types.map(t => t.type.name).join(", ")}</p>
+      `;
+    } catch (err) {
+      errorMsg.textContent = err;
+    }
+  };
+  
